2e0210644d5c3ed3cc735ab1d9d30e8e0adbbeea
alightnin <alightnin@gmail.com>
2012-09-07 23:28:18 -0600

    Ajax Intro
    Getting error on loading the index.html for the div container.
    
    XMLHttpRequest cannot load
    file://localhost/Users/craigweir/Tutorial/index.html. Origin null is
    not allowed by Access-Control-Allow-Origin.

--- android.js
+++ android.js
@@ -1,10 +1,22 @@
-
 $(document).ready(function(){ 
-    $('#header ul').addClass('hide'); 
-    $('#header').append('<div class="leftButton" 
-        onclick="toggleMenu()">Menu</div>'); 
+    loadPage();
 });
-function toggleMenu() { 
-    $('#header ul').toggleClass('hide'); 
-    $('#header .leftButton').toggleClass('pressed'); 
+function loadPage(url) {
+	$('body').append('<div id="progress">Loading...</div>');
+	scrollTo(0,0);
+   	if (url == undefined) {
+      	$('#container').load('index.html #header ul', hijackLinks);
+   	} else {
+      	$('#container').load(url + ' #content', hijackLinks);
+   	}
+}
+function hijackLinks() {
+    $('#container a').click(function(e){
+        e.preventDefault();
+        loadPage(e.target.href);
+        var title = $('h2').html() || 'Hello!';
+		$('h1').html(title);
+		$('h2').remove();
+    });
+    $('#progress').remove();
 }
